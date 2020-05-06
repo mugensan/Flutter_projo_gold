dabcb43903a160deafdcf8a924c28a5e1efd8b47
krodi001 <kev.roditi@gmail.com>
2020-05-06 15:35:51 +0200

    handling auth failures

---
+++ lib/domain/auth/auth_failure.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'auth_failure.freezed.dart';
+
+///WHAT KIND OF ERRORS CAN WE IMAGINE HAPPENING
+///1. user cancels the sign in (google)
+///2. error with server
+///3. duplicates
+///4. invalid combo of @$$pass
+///
+@freezed
+abstract class AuthFailure with _$AuthFailure {
+  const factory AuthFailure.cancelledByUser() = CancelledByUser;
+  // Serves as a "catch all" failure if we don't know what exactly went wrong
+  const factory AuthFailure.serverError() = ServerError;
+  const factory AuthFailure.emailAlreadyInUse() = EmailAlreadyInUse;
+  const factory AuthFailure.invalidEmailAndPasswordCombination() =
+  InvalidEmailAndPasswordCombination;
---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutterstaggeredgridontap/domain/auth/value_objects.dart';
+import 'package:flutterstaggeredgridontap/domain/auth/auth_failure.dart';
+
+///INTERFACE with facade (design pattern) used to connect 2 or more classes with weird facades
+///also 1 class for google and firebase auth
+
+abstract class IAuthFacade{
+
+  //REGISTER
+Future<Either<AuthFailure,Unit>>registerWithEmailAndPassword({
+  //USING THE CREATED VALIDATORS
+
+  @required EmailAddress emailAddress,
+  @required Password password,
+});
+
+//SIGN IN
+Future<Either<AuthFailure,Unit>>signInWithEmailAndPassword({
+  //USING THE CREATED VALIDATORS
+  @required EmailAddress emailAddress,
+  @required Password password,
+});
+
+//GOOGLE SIGN IN
+Future<Either<AuthFailure,Unit>>signInWithGoogle();
+}
